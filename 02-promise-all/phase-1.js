a820a676014e9cf3ec5e659a95bb79afc4f36029
tristan <thuckabee.work@gmail.com>
2023-11-06 18:29:45 -0600

    02 done

--- 02-promise-all/phase-1.js
+++ 02-promise-all/phase-1.js
@@ -27,9 +27,12 @@ function liftWeights() {
 
 function workout() {
   // refactor this code to use Promise.all
-  stretch()
-    .then(runOnTreadmill)
-    .then(liftWeights)
+  // stretch()
+  //   .then(runOnTreadmill)
+  //   .then(liftWeights)
+  //   .then(() => console.log("done working out"))
+  //   .catch((err) => console.log(err));
+  Promise.all([ stretch(), runOnTreadmill(), liftWeights() ])
     .then(() => console.log("done working out"))
     .catch((err) => console.log(err));
 }
